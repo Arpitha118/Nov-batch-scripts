730483b4cd6595276606a1a96c26c4221d31658a
Arpitha118 <rarpitha40@gmail.com>
2024-12-23 18:39:16 +0530

    Updated code

---
+++ Operators.js
@@ -0,0 +1,9 @@
+console.log(10+20);
+console.log(10-5);
+console.log(10*5);
+console.log(10/5);
+console.log(10%5);
+var a = 10;
+console.log(a++);//Post Incrementaion - 10 but internally a is 11 
+console.log(a);//a = 11
+console.log(++a);//Pre-Incrementation -12
