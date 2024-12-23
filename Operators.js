0d09a24fc0f6ac4a8d0e8b2957d3cb6f7b63fe53
Arpitha118 <rarpitha40@gmail.com>
2024-12-23 18:52:11 +0530

    Modified Operators file

--- Operators.js
+++ Operators.js
@@ -8,3 +8,13 @@ var a = 10;
 console.log(a++);//Post Incrementaion - 10 but internally a is 11 
 console.log(a);//a = 11
 console.log(++a);//Pre-Incrementation -12
+
+
+//== checks only value not data type
+console.log(10 == '10');//true
+console.log(10 == 10);//true
+
+//=== checks value and data type both
+console.log(10 === '10');//false
+console.log(10 === 10);//true
+
