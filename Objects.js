a6b52bd684575021ccbc898b1165b43e24a12647
Arpitha118 <rarpitha40@gmail.com>
2024-12-24 10:47:21 +0530

    Objects file

---
+++ Objects.js
@@ -0,0 +1,22 @@
+//Data Types: Object, String, number, boolean, null, undefined
+//Objects:
+//1. JSON Object {key:value pairs}
+//2. Array Object [ele1, ele2, ele3...]
+//3. Functions
+
+
+const s1 = {
+    Name: "Priya",
+    Age: 24,
+    Fees_paid: 3400.70,
+    Work_Experience: {
+        ABC: '3 Months',
+        XYZ: '2 Months'
+    },
+    Marks: {
+        Maths: 45,
+        Science: 55,
+    },
+    Percentage: ['Sem-I 84','Sem-II 86','Sem-III 88'],
+    Performance: ()=>console.log('Good',)
+}
