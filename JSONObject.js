aade9c2a365da67460e9b037d3e929bbeeaafbbe
Arpitha118 <rarpitha40@gmail.com>
2024-12-24 10:34:01 +0530

    JSONObjects file

---
+++ JSONObject.js
@@ -0,0 +1,21 @@
+// s1 --> Name, age
+// s2 --> Name, Email
+// s3 --> Name, Work_Experience [XYZ: 3 months, ABC: 12]
+
+const s1 = {
+    Name: "Priya",
+    Age: 24,
+}
+
+const s2 = {
+    Name:"Ankit",
+    Email:'ankit@kod.com',
+}
+
+const s3 = {
+    Name:"Akash",
+    Work_Experience : {
+        XYZ: '3 Months',
+        ABC: '12 Months'
+    }
+}
