adbdbdb9a3da916a2ee61e27aa90aa06fa6172a2
Arpitha118 <rarpitha40@gmail.com>
2024-12-24 10:12:15 +0530

    DataTypes file

---
+++ DataTypes.js
@@ -0,0 +1,23 @@
+//number Data Types: All numbers (whole, decimal, positive, negative)
+var a = 12346789.34568;
+console.log(a, typeof a);
+
+//Infinity
+var b = 456/0;
+console.log(b, typeof b);
+
+//Strings: Single character or series of characters all are strings
+var c = 'Kodnest'
+console.log(c, typeof c);
+
+//Boolean: true / false
+var d = true;
+console.log(d, typeof d);
+
+//undefined
+let e;
+console.log(d);//undefined
+
+//null
+let f = null;
+console.log(e);
